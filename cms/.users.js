e537e5e6d62121f6158a1a21fb40586798e105ff
ubuntu <ubuntu@localhost.localdomain>
2018-04-14 03:12:22 +0800

    make image

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '21347bac5c4174f165952605b6b4fb13',
+			hash: 'e19c32b1014c5af4e38608e98780cf76912a5c09d02328546bde96639c074789',
+			user_created_timestamp: 1523643125078
+		}
+	]
+}
---
+++ kk/blog/cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '3332f19c5d1f90b3f3d240962befca9f',
+			hash: '12c95b16402c0c71126742a55fa25a6eb401a2ab72f23cd04a7eb4ef12e2c747',
+			user_created_timestamp: 1523644210451
+		}
+	]
+}
